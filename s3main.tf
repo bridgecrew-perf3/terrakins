d6a49eaf51d706b5d39ff2143cea304d928b6e4a
Devpro28 <87431341+Devpro28@users.noreply.github.com>
2021-07-22 19:58:19 -0400

    first commit

---
+++ s3main.tf
@@ -0,0 +1,25 @@
+#s3main.tf
+variable "aws_region" {
+  type = string
+}
+
+variable "domain_name" {
+  type = string
+}
+
+provider "aws" {
+  region = var.aws_region
+  version = "~> 2.52"
+}
+
+module "website" {
+  source = "./Web"
+  domain_name = var.domain_name
+}
+
+resource "aws_s3_bucket_object" "object" {
+  bucket = var.domain_name
+  key    = "index.html"
+  source = ".files/index.html"
+
+depends_on = "aws_s3_bucket.website"
---
+++ web/variables.tf
@@ -0,0 +1,4 @@
+# Web/variables.tf
+variable "domain_name" {
+  type = string
+}
