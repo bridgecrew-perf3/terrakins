afa7fdb0209dd62cae592acc8924a5fa0254556f
Devpro28 <87431341+Devpro28@users.noreply.github.com>
2021-07-30 18:47:08 -0400

    hi def

--- files/test.groovy
+++ files/test.groovy
@@ -3,11 +3,12 @@ package com.latch.aws
 pipeline {
     agent any 
 
-  environment {
-    AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
-    AWS_SESSION_TOKEN = credentials ('AWS_SESSION_TOKEN')
+    environment {
+      AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY_ID')
+      AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
+      AWS_SESSION_TOKEN = credentials ('AWS_SESSION_TOKEN')
   }
+    stages {
           script {
             sh '/usr/local/bin/aws s3 sync /Users/cedricniamba/Desktop/s3test/. s3://testop1'
           }
